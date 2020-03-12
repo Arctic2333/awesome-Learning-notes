a4ade59f111a61dae209c7801f91648442a60387
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-03-12 21:29:07 +0800

    link_list

---
+++ data_structure/list/java/StuLinkList.java
@@ -0,0 +1,8 @@
+public class StuLinkList {  // 定义链表类 StuLinkList
+    // 链表头节点
+    private StuNode head;
+    // 构造方法，新建链表将传递进来的结点对象设置为链表头结点
+    public StuLinkList( StuNode hd ){
+        head = hd;
+    }
+}
--- data_structure/list/java/StuNode.java
+++ data_structure/list/java/StuNode.java
@@ -1,3 +1,22 @@
-public class StuNode {
+public class StuNode {  // 链表节点
+    // 数据域为学生对象
+    private Student student;
+    // 引用域指向下一个节点
+    private StuNode nextlink;
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public StuNode getNextlink() {
+        return nextlink;
+    }
+
+    public void setNextlink(StuNode nextlink) {
+        this.nextlink = nextlink;
+    }
 }
