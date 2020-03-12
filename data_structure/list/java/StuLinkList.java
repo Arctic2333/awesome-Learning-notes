69d7fdff155b537639b11582fe106712887aa60b
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-03-12 22:03:28 +0800

    link_list

--- data_structure/list/java/StuLinkList.java
+++ data_structure/list/java/StuLinkList.java
@@ -1,8 +1,42 @@
 public class StuLinkList {  // 定义链表类 StuLinkList
     // 链表头节点
     private StuNode head;
+
     // 构造方法，新建链表将传递进来的结点对象设置为链表头结点
-    public StuLinkList( StuNode hd ){
+    public StuLinkList(StuNode hd) {
         head = hd;
     }
+
+    public void insertAfter(StuNode p, StuNode s) {
+        s.setNextlink(p.getNextlink());
+        p.setNextlink(s);
+    }
+
+    public void insertBefore(StuNode p, StuNode s) {
+        if (head == null)
+            return;
+        StuNode q = head;
+        while (q.getNextlink() != null) {
+            if (q.getNextlink() == p) {
+                q.setNextlink(s);
+                s.setNextlink(p);
+                break;
+            }
+            q = q.getNextlink();
+        }
+    }
+
+    public void delete(StuNode p) {
+        if (head == null)
+            return;
+        StuNode q = head;
+        while (q.getNextlink() != null) {
+            if (q.getNextlink() == p) {
+                q.setNextlink(p.getNextlink());
+            }
+            q = q.getNextlink();
+        }
+    }
+
+
 }
