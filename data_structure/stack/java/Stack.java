940cc4ee3db5130ced3b9a03e9d39c06580755a6
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-03-21 11:27:53 +0800

    StackArray

---
+++ data_structure/stack/java/Stack.java
@@ -0,0 +1,13 @@
+public interface Stack {
+    public int getSize(); // 返回堆栈的大小
+
+    public boolean isEmpty(); // 判断栈空
+
+    public void push(Object e);  // 入栈
+
+    public Object pop() throws StackEmptyException;  // 栈顶元素出栈
+
+    public Object peek() throws StackEmptyException;  // 取栈顶元素
+
+
+}
---
+++ data_structure/stack/java/StackArray.java
@@ -0,0 +1,48 @@
+public class StackArray implements Stack {
+    private final int LEN = 8;  // 数组的默认大小
+    private Object[] elements; // 数据元素数组
+    private int top; // 栈顶指针
+
+    public StackArray() {
+        top = -1;
+        elements = new Object[LEN];
+    }
+
+    // 返回栈的大小
+    public int getSize() {
+        return top + 1;
+    }
+
+    // 判断栈是否为空
+    public boolean isEmpty() {
+        return top < 0;
+    }
+
+    // 入栈
+    public void push(Object e) {
+        if (getSize() >= elements.length)
+            expandSpace();
+        elements[++top] = e;
+    }
+
+    private void expandSpace() {
+        Object[] a = new Object[elements.length * 2];
+        for (int i = 0; i < elements.length; i++)
+            a[i] = elements[i];
+        elements = a;
+    }
+
+    public Object pop() throws StackEmptyException {
+        if (getSize() < 1)
+            throw new StackEmptyException("错误，堆栈为空");
+        Object obj = elements[top];
+        elements[top--] = null;  // elements[top] = null;  top -= 1;
+        return obj;
+    }
+
+    public Object peek() throws StackEmptyEcxception {
+        if (getSize() < 1)
+            throw new StackEmptyException("错误，堆栈为空");
+        return elements[top];
+    }
+}
