16d043af914c7271d497749a9118b34ec6e291e7
苏淞丰 <50102735+Arctic2333@users.noreply.github.com>
2020-03-22 15:07:56 +0800

    Stack

---
+++ data_structure/stack/src/stack/TestStack.java
@@ -0,0 +1,13 @@
+package stack;
+import java.util.Stack;
+public class TestStack {
+    private String teststring ;
+    private Stack<Character> stack ;
+    public TestStack(String teststring){
+        this.teststring = teststring;
+        this.stack = new Stack<>();
+    }
+    private void analysisString(){
+
+    }
+}
